91b45607730ff38c4c52cdcf9942bc986dfb06a1
Yurii Kniazyk <y.kniazyk@gmail.com>
2020-07-20 22:41:28 +0300

    init

---
+++ HW4/HW4/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace HW4
+{
+    public class Person
+    {
+        private string _name;
+        private DateTime _birthday;
+
+        public string Name => _name;
+        public DateTime Birthday => _birthday;
+
+        public Person() { }
+        public Person(string name, DateTime birth)
+        {
+            _name = name;
+            _birthday = birth;
+        }
+
+        public void Input(string name, DateTime birthday)
+        {
+            _name = name;
+            _birthday = birthday;
+        }
+
+        public int Age()
+        {
+            int age = DateTime.Today.Year - _birthday.Year;
+            if (_birthday.DayOfYear > DateTime.Today.DayOfYear)
+            {
+                age--;
+            }
+
+            return age;
+        }
+
+        public bool ChangeName(string name = "Very Young")
+        {
+            if (Age() < 16)
+            {
+                _name = name;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Output() => ToString();
+
+        public static bool operator ==(Person firstPerson, Person secondPerson)
+        {
+            return (firstPerson?._name == secondPerson?._name);
+        }
+
+        public static bool operator !=(Person firstPerson, Person secondPerson)
+        {
+            return !(firstPerson?._name == secondPerson?._name);
+        }
+
+        public override string ToString()
+        {
+            return $"Name person: {_name} Age person: {Age()}";
+        }
+
+    }
+}
---
+++ HW4/HW4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new List<Person>
+            {
+                new Person("Yurii", new DateTime(2000, 10,10)),
+                new Person("Yurii", new DateTime(2001, 10,10)),
+                new Person("Sasha", new DateTime(2005, 10,10)),
+                new Person("Sasha", new DateTime(2001, 10,10)),
+                new Person("Masha", new DateTime(2015, 10,10)),
+                new Person("Baba", new DateTime(2000, 10,10)),
+            };
+            //var persons = new List<Person>();
+            //const int  COUNT = 6;
+
+            //for (int i = 0; i < COUNT; i++)
+            //{
+            //    Console.Write("Enter name person: ");
+            //    var name = Console.ReadLine();
+            //    Console.Write("Enter birthday: ");
+            //    var birthday = Convert.ToDateTime(Console.ReadLine());
+
+            //    persons.Add(new Person(name, birthday));
+            //}
+            Console.WriteLine("All persons");
+
+            persons.ForEach(item => Console.WriteLine(item.ToString()));
+            Console.WriteLine();
+
+            Console.WriteLine("Change name person which age < 16");
+            persons.ForEach(item =>
+            {
+                item.ChangeName();
+                Console.WriteLine(item.ToString());
+            });
+            Console.WriteLine();
+
+            Console.WriteLine("Persons with the same name:");
+            for (int i = 0; i < persons.Count; i++)
+            {
+                for (int j = i + 1; j < persons.Count; j++)
+                {
+                    if (persons[i] == persons[j])
+                    {
+                        Console.WriteLine($"{persons[i].Name} and {persons[j].Name} are equals");
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
